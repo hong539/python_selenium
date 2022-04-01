96b23e27181f5773fe6b354c799329d1f4562e27
hong539 <j50508@gmail.com>
2022-04-01 08:17:59 +0800

    Update codes

--- test_cases/multi_urls.py
+++ test_cases/multi_urls.py
@@ -2,25 +2,16 @@ from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 import time
 
-driver = webdriver.Chrome(executable_path=r"D:/Python_Projects/chromedriver_win32/chromedriver.exe")
+driver = webdriver.Chrome(executable_path=r"webdriver_loaction_path/chromedriver.exe")
 
 
 driver.maximize_window()
 driver.delete_all_cookies()
 
-urls = ["https://www.boce.com/dns/m.heibai360.com",
-        "https://www.boce.com/dns/hbzb666.com",
-        "https://www.boce.com/dns/heibai688.com",
-        "https://www.boce.com/dns/hbzb999.com",
-        "https://www.boce.com/dns/heibaikankan.com",
-        "https://www.boce.com/dns/heibai999.com",
-        "https://www.boce.com/dns/myeogm.com",
-        "https://www.boce.com/dns/13cskppe.com",
-        "https://www.boce.com/dns/xilufund.com",
-        "https://www.boce.com/dns/cqlwny.com",
-        "https://www.boce.com/dns/xtuike.com",
-        "https://www.boce.com/dns/imiaoge.com",
-        "https://www.boce.com/dns/lfctc.com"]
+urls = [
+
+        ]
+
 for posts in range(len(urls)):
     print(posts)
     driver.get(urls[posts])    
